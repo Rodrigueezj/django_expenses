53c859ba6e3d343ebc53508f7b48b26285188017
Juan David <jdbelloro@unal.edu.co>
2023-05-25 20:26:09 -0500

    postresql

--- expenses/settings.py
+++ expenses/settings.py
@@ -77,8 +77,18 @@ WSGI_APPLICATION = 'expenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db-expenses',
+        'USER': 'juandb',
+        'PASSWORD': 'Cod2023b!',
+        'HOST': 'db-expenses.cjkamheclfbm.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
+        
+    },
+    'old_default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
