6101a9e9a60db2c66b2a070882a99aebd832bda4
Juan David <jdbelloro@unal.edu.co>
2023-06-12 16:27:14 -0500

    static_root

--- expenses/settings.py
+++ expenses/settings.py
@@ -145,7 +145,9 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
