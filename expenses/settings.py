d6df9c5ea32afd9e36a6862b8a98e9d227e30032
Juan David <jdbelloro@unal.edu.co>
2023-06-12 16:32:40 -0500

    static files

--- expenses/settings.py
+++ expenses/settings.py
@@ -143,17 +143,17 @@ LOGIN_REDIRECT_URL = reverse_lazy('BASE')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AWS_ACCESS_KEY_ID = S3_KEY_ID
 AWS_SECRET_ACCESS_KEY = S3_SECRET_KEY_ID
 AWS_STORAGE_BUCKET_NAME = 'expensesbucket'
