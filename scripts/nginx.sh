f9da112ef1bc65595322a1916f279a82e20f3a3d
Juan David <jdbelloro@unal.edu.co>
2023-06-12 18:23:01 -0500

    more and more bugsss

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/bash
 
 sudo systemctl daemon-reload
@@ -6,8 +5,6 @@ sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/django_expenses/nginx/nginx.conf /etc/nginx/sites-available/expenses
 sudo ln -s /etc/nginx/sites-available/expenses /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/expenses /etc/nginx/sites-enabled
-#sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
