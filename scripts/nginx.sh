8553cc9e8c4b0f0648896ae1dd8472b077a1f2ca
Juan David <jdbelloro@unal.edu.co>
2023-06-12 18:46:53 -0500

    x_x help please

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -7,4 +7,5 @@ sudo cp /home/ubuntu/django_expenses/nginx/nginx.conf /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/expenses /etc/nginx/sites-enabled/
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
-
+sudo service gunicorn restart
+sudo service nginx restart
