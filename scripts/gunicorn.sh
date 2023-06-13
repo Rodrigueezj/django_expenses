b4639d72b69e1e4c9cb41a67aa931d2d129eed7d
Juan David <jdbelloro@unal.edu.co>
2023-06-12 19:25:00 -0500

    gunicorn issues

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -2,5 +2,5 @@
 sudo cp /home/ubuntu/django_expenses/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/django_expenses/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
-sudo systemctl start gunicorn.socket
-sudo systemctl enable gunicorn.socket
+sudo systemctl start gunicorn.service
+sudo systemctl enable gunicorn.service
